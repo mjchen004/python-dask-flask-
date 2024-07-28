1600ddd5db2bb8f1f56f610bfd65ed48f5e2537a
Leon Mao <leoneric0212@gmail.com>
2024-07-28 21:58:38 +0800

    更新匯入資料、顯示地圖及長條圖

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -1,99 +1,127 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash import Dash, dcc, html,dash_table,Input, Output,callback
+# from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
-app1 = dash.Dash(__name__,requests_pathname_prefix='/dashboard/dashboard2/')
+app1 = Dash(__name__,requests_pathname_prefix='/dashboard/dashboard2/')
 
-data = {
-    '日期': ['2023-01-01', '2023-01-02', '2023-01-03'],
-    '天氣': ['晴天', '雨天', '陰天'],
-    '地區': ['北部', '中部', '南部'],
-    '經度': [121.5, 120.5, 121.0],
-    '緯度': [25.0, 24.0, 23.5],
-    '事故數': [10, 5, 8]
-}
-df = pd.DataFrame(data)
+df = pd.read_csv(f"./data/2018.csv",encoding='utf-16')
+df.columns=df.columns.str.strip()
+df[['死亡人數', '受傷人數']] = df['死亡受傷人數'].str.extract('死亡(\d+);受傷(\d+)').astype(int)
 
 # Define the app layout
 app1.layout = html.Div([
     html.Div([
+        html.H1("交通事故分析"),
         html.Div([
+            dcc.DatePickerSingle(
+                id='date-picker',
+                date=df['發生日期'].min(),
+                display_format='YYYY-MM-DD',
+                placeholder="選擇日期",
+            ),
+            html.Hr(),
             dcc.Checklist(
                 id='weather-filter',
-                options=[{'label': weather, 'value': weather} for weather in df['天氣'].unique()],
-                value=df['天氣'].unique().tolist(),
+                options=[{'label': weather, 'value': weather} for weather in df['天候名稱'].unique()],
+                value=df['天候名稱'].unique().tolist(),
                 labelStyle={'display': 'inline-block'}
             ),
+            html.Hr(),
             dcc.Checklist(
                 id='region-filter',
-                options=[{'label': region, 'value': region} for region in df['地區'].unique()],
-                value=df['地區'].unique().tolist(),
+                options=[{'label': region, 'value': region} for region in df['發生地點'].unique()],
+                value=df['發生地點'].unique().tolist(),
                 labelStyle={'display': 'inline-block'}
             ),
-            dcc.DatePickerRange(
-                id='date-picker',
-                start_date=df['日期'].min(),
-                end_date=df['日期'].max(),
-                display_format='YYYY-MM-DD',
-                start_date_placeholder_text="選擇開始日期",
-                end_date_placeholder_text="選擇結束日期",
+            html.Hr(),
+            dcc.Checklist(
+                id='lights-filter',
+                options=[{'label': light, 'value': light} for light in df['光線名稱'].unique()],
+                value=df['光線名稱'].unique().tolist(),
+                labelStyle={'display': 'inline-block'}
             ),
+            
             dcc.Graph(id='map', style={'height': '500px'}),
         ], style={'width': '100%', 'display': 'inline-block', 'vertical-align': 'top'}),
     ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
     
     html.Div([
-        html.H3('Filtered Data'),
-        html.Ul(id='filtered-data-list'),
-    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
+        html.H3('事故資料表'),
+        dash_table.DataTable(id='filtered-data-list',
+                             columns=[{'name':col,'id':col} for col in ['發生日期','發生地點','事故類別名稱','天候名稱','光線名稱','速限_第1當事者','道路類別_第1當事者_名稱','死亡人數','受傷人數']],
+                             data=df.to_dict('records'),
+                             page_size=10,
+                             style_cell_conditional=[{
+                                 'if': {'column_id': '光線名稱'}, 'width': '250px'}])]),
     
     html.Div([
-        dcc.Graph(id='scatter-plot', style={'height': '500px'}),
+        dcc.Graph(id='bar-chart', style={'height': '500px'}),
     ], style={'width': '100%', 'display': 'inline-block', 'vertical-align': 'top'}),
+    
+    html.Div([
+        dcc.Graph(id='bar-chart-2', style={'height': '500px'}),
+    ], style={'width': '100%', 'display': 'inline-block', 'vertical-align': 'top'})
 ])
 
 @app1.callback(
-    [Output('map', 'figure'),
-     Output('filtered-data-list', 'children'),
-     Output('scatter-plot', 'figure')],
-    [Input('weather-filter', 'value'),
+    [Output('filtered-data-list', 'data'),
+     Output('map', 'figure'),
+     Output('bar-chart', 'figure'),
+     Output('bar-chart-2', 'figure')],
+    [Input('date-picker', 'date'),
+     Input('weather-filter', 'value'),
      Input('region-filter', 'value'),
-     Input('date-picker', 'start_date'),
-     Input('date-picker', 'end_date')]
+     Input('lights-filter', 'value')
+     ]
 )
 
-def update_output(selected_weathers, selected_regions, start_date, end_date):
-    # Filter data based on user inputs
-    filtered_df = df[
-        (df['天氣'].isin(selected_weathers)) &
-        (df['地區'].isin(selected_regions)) &
-        (df['日期'] >= start_date) &
-        (df['日期'] <= end_date)
-    ]
+def update_output(selected_date,selected_weathers, selected_regions, selected_lights):
+    if selected_date is not None:
+        year=selected_date.split('-')[0]
+        file_name=f'./data/{year}.csv'
+    df=pd.read_csv(file_name,encoding='utf-16')
+    df.columns=df.columns.str.strip()
+    df[['死亡人數', '受傷人數']] = df['死亡受傷人數'].str.extract('死亡(\d+);受傷(\d+)').astype(int)
+
+    filtered_df=df[(df['發生日期']==selected_date) &
+                   (df['天候名稱'].isin(selected_weathers)) &
+                   (df['發生地點'].isin(selected_regions)) &
+                   (df['光線名稱'].isin(selected_lights))]
     
-    print(filtered_df)  # Print filtered data for debugging
+    count=len(filtered_df)  #計算案件數量
     
-    # Convert filtered data to a list of html.Li elements
-    filtered_data_list = [
-        html.Li(f"日期: {row['日期']}, 天氣: {row['天氣']}, 事故數: {row['事故數']}")
-        for idx, row in filtered_df.iterrows()
-    ]
+    map_figure = px.scatter_mapbox(filtered_df,
+                                   lat="緯度",
+                                   lon="經度",
+                                   hover_name="發生地點",
+                                   color="天候名稱",
+                                   zoom=6,
+                                   labels="事故地圖")
+    map_figure.update_layout(mapbox_style="open-street-map",
+                             margin={"r":0,"t":80,"l":100,"b":50},
+                             title={"text":"事故地圖",
+                                    'x': 0.5,  # 標題居中
+                                    "xanchor":"center",
+                                    'y': 0.9,
+                                    'font': {'size': 30}
+                                    })
+    location_counts = filtered_df['發生地點'].value_counts().reset_index()
+    location_counts.columns = ['發生地點', '案件數量']
+    bar_figure1=px.bar(location_counts,
+                      x='發生地點',
+                      y='案件數量',
+                      labels={'發生地點': '發生地點', '案件數量': '案件數量'},
+                      title=f'各縣市案件數量', 
+                      height=500)
     
-    # Update map figure
-    if filtered_df.empty:
-        fig_map = px.scatter_mapbox(lat=[0], lon=[0], zoom=1)
-        fig_map.update_layout(mapbox_style="open-street-map")
-    else:
-        fig_map = px.scatter_mapbox(filtered_df, lat='緯度', lon='經度', color='事故數',
-                                    hover_name='地區', hover_data=['日期', '天氣'],
-                                    zoom=5, height=500)
-        fig_map.update_layout(mapbox_style="open-street-map")
-    
-    # Update scatter plot figure (dummy data for illustration)
-    fig_scatter = px.scatter(df, x='日期', y='事故數', color='天氣', hover_data=['地區'], title='Scatter Plot')
-    
-    print(fig_scatter)  # Print fig_scatter for debugging
-    
-    return fig_map, filtered_data_list, fig_scatter
+    road_counts = filtered_df['天候名稱'].value_counts().reset_index()
+    road_counts.columns = ['天候名稱', '案件數量']
+    bar_figure2=px.bar(road_counts,
+                      x='天候名稱',
+                      y='案件數量',
+                      labels={'天候名稱': '天候名稱', '案件數量': '案件數量'},
+                      title=f'各縣市案件數量', 
+                      height=500)
+
+    return filtered_df.to_dict('records'), map_figure,bar_figure1,bar_figure2
